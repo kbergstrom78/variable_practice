f93514f989a4b2e0784beea2c0dfd3b5af50db47
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-02-22 14:26:57 -0700

    Fix syntax in line 9 and update puts commands

--- variables.rb
+++ variables.rb
@@ -6,11 +6,12 @@ number_of_dogs = 2
 year_married = 2021
 shoe_size = 7.5
 height_in_inches = 66.5
-tax_refund = .02
+tax_refund = 0.02
 emptied_dishwasher = true
 vacuumed = true
 cleaned_bathroom = false
 name = Kim
 location = Los Alamos
-puts You deserve a snack, name
-puts How is the weather in location?
+puts name
+puts location
+puts year_married
