3a8a406800578e6779924a613f02bb178af04d0b
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-02-22 14:23:29 -0700

    Add puts commands

--- variables.rb
+++ variables.rb
@@ -10,3 +10,7 @@ tax_refund = .02
 emptied_dishwasher = true
 vacuumed = true
 cleaned_bathroom = false
+name = Kim
+location = Los Alamos
+puts You deserve a snack, name
+puts How is the weather in location?
